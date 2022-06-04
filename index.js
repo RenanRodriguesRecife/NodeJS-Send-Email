d06334c7f9d348fc1dfb4330e60f7a2df825dede
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-04 12:28:37 -0300

    configurações iniciais

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const nodemailer = require('nodemailer');
+
+
+const transport = nodemailer.createTransport({
+    //pesquise sntp + o nome do serviço de email
+    //para verificar o host e a porta
+    host: '',
+    port: 0,
+    secure: false, //true para porta 465 e false para outras
+    auth:{
+        user: 'email@email.com',
+        pass: 'passworld',
+    }
+})
+
